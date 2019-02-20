8c14d9d8d3dafde225ba05252f138a159495c4a4
miranov25 <marian.ivanov@cern.ch>
2019-02-20 12:44:02 +0100

    PWGPP-485- parser test of widget string

--- JIRA/PWGPP-485/parserTest.py
+++ JIRA/PWGPP-485/parserTest.py
@@ -1,18 +1,20 @@
-import pyparsing # make sure you have this installed
+import pyparsing  # make sure you have this installed
+
 
 def test0():
-    thecontent = pyparsing.Word(pyparsing.alphanums) | '+' | '-'
-    parens     = pyparsing.nestedExpr( '(', ')', content=thecontent)
-    res = parens.parseString("((12 + 2) + 3)")
-    print(res.asList())
+    theContent = pyparsing.Word(pyparsing.alphanums) | '+' | '-'
+    parents = pyparsing.nestedExpr('(', ')', content=theContent)
+    result = parents.parseString("((12 + 2) + 3)")
+    print(result.asList())
+
 
 # test to parse widget configuration string:
-toParse0="slider.P1(0+1,1,0.5,0,1),slider.commonF1(0,15,5,0:5),"
-toParse0+="accordion.acc2(slider.P2(0+1,1,0.5,0,1),slider.commonF2(0,15,5,0:5)),"
-toParse0+="accordion.acc3(slider.P3(0,1,0.5,0,1),slider.commonF3(0,15,5,0,5))"
-toParse0="("+toParse0+")"
+toParse0 = "slider.P1(0+1,1,0.5,0,1),slider.commonF1(0,15,5,0:5),"
+toParse0 += "accordion.acc2(slider.P2(0+1,1,0.5,0,1),slider.commonF2(0,15,5,0:5)),"
+toParse0 += "accordion.acc3(slider.P3(0,1,0.5,0,1),slider.commonF3(0,15,5,0,5))"
+toParse0 = "(" + toParse0 + ")"
 
-thecontent = pyparsing.Word(pyparsing.alphanums+".+-") | '#' | ','  | ':'
-parens     = pyparsing.nestedExpr( '(', ')', content=thecontent)
-res = parens.parseString(toParse0)
+theContent = pyparsing.Word(pyparsing.alphanums + ".+-") | '#' | ',' | ':'
+parents = pyparsing.nestedExpr('(', ')', content=theContent)
+res = parents.parseString(toParse0)
 res.asList()
