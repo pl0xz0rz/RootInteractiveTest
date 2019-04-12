2c82dc47e5652cd591751b950ca313cf18d69c82
miranov25 <marian.ivanov@cern.ch>
2019-04-12 16:02:31 +0200

    PWGPP-485-, PWGPP-520 -adding test speed of the C++ - python interface * Root and python calculation of the matrix determinant

--- JIRA/PWGPP-485/testBokehDraw.C
+++ JIRA/PWGPP-485/testBokehDraw.C
@@ -1,12 +1,19 @@
 /*
  .L $HOME/github/RootInteractiveTest/JIRA/PWGPP-485/testBokehDraw.C
+
+ initPython()
+
  */
 TString importBokeh="";
 TTree * tree=0;
-void InitBokeh() {
+TMatrixD *testMatrix=0;
+TStopwatch timer;
+
+void initPython() {
   importBokeh  = "from Tools.aliTreePlayer import * \n";
   importBokeh += "from anytree import * \n";
   importBokeh += "from InteractiveDrawing.bokeh.bokehDraw import *\n";
+  importBokeh += "import numpy as np\n";
 }
 
 void InitData(){
@@ -28,5 +35,23 @@ void treeBokehDraw(const char * treeName ,const char *varX, const char *vary, co
   TString x=importBokeh;
   x+="tree=ROOT.gROOT.GetGlobal(\"tree\") \n";
   x+=TString::Format("");
+}
 
+
+void testEvalMatrix(Int_t m, Bool_t doPrint, Int_t nLoops=1000){
+  testMatrix=new TMatrixD(m,m);
+  for (Int_t i=0;i<m; i++)
+    for (Int_t j=0;j<m; j++)
+      (*testMatrix)(i,j)=gRandom->Rndm();
+  TString sumP=R"raw(m=ROOT.gROOT.GetGlobal("testMatrix")\n)raw";
+  if (doPrint){
+    sumP += "print(m.Determinant())";
+  }else {
+    sumP += "m.Determinant()";
+  }
+  printf("Python loop\n");
+  timer.Start(); for (Int_t i=0; i<nLoops;i++)  {(*testMatrix)(0,0)+=1;TPython::Exec(sumP);} timer.Print();
+
+  printf("C loop\n");
+  timer.Start(); for (Int_t i=0; i<nLoops;i++)  {(*testMatrix)(0,0)+=1;testMatrix->Determinant();} timer.Print();
 }
