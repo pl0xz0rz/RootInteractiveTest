a5eb2e91955bf2d0277e5ab87ecef1fcc74630dd
miranov25 <marian.ivanov@cern.ch>
2019-06-11 11:55:50 +0200

    PWGPP-527 - merging huge THn

--- JIRA/PWGPP-527/histoToTree.C
+++ JIRA/PWGPP-527/histoToTree.C
@@ -24,3 +24,33 @@ void histoToTree(){
   delete pcstream;
 
 }
+
+void mergeBigHn(){
+  TObjArray* flist = gSystem->GetFromPipe("cat mc.list").Tokenize("\n");
+  THn *his = 0;
+  //for (Int_t i=0; i<flist->GetEntries(); i++){
+  for (Int_t i=0; i<2; i++){
+    printf("%s\n",flist->At(i)->GetName());
+    TFile * f=TFile::Open(flist->At(i)->GetName());
+    THnSparseF* hisAdd0 = (THnSparseF*)f->Get("hR_phi_Z_pT");
+    // first project pt 0-2 GeV
+    Int_t projection[4]={0,1,2,3};
+    Int_t rebin2[4]={2,2,10,10};
+    hisAdd0->GetAxis(3)->SetRangeUser(0,2);
+    THnSparseF* hisAdd1=(THnSparseF*)hisAdd0->ProjectionND(4,projection);
+    delete hisAdd0;
+    THnSparseF* hisAdd2=(THnSparseF*)hisAdd1->Rebin(rebin2);
+    delete hisAdd1;
+    if (his==NULL){
+      his=THn::CreateHn("merge","merge", hisAdd2);
+    }else{
+      THn* hisTMP=THn::CreateHn("merge","merge", hisAdd2);
+      his->Add(hisTMP);
+      delete hisTMP;
+    }
+    delete hisAdd2;
+  }
+  TFile *fout = TFile::Open("mcMerged.root","recreate");
+  his->Write("hR_phi_Z_pT");
+  fout->Close();
+}
