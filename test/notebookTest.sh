d2e8c6fdad08191138a8266f4d37b6a856608882
miranov25 <marian.ivanov@cern.ch>
2019-06-14 23:57:32 +0200

    PWGPP-532 - clean notebook without reformating

--- test/notebookTest.sh
+++ test/notebookTest.sh
@@ -1,1 +1,11 @@
-py.test --nbval $(ls */*ipynb | grep -v 460) | tee pytest.log 
+#clean_ipynb */*/*ipynb
+for note in $(ls */*/*ipynb); do
+    nb-clean clean < ${note}  > ${note}2
+    echo mv ${note}2 ${note}
+    mv ${note}2 ${note}
+done
+
+py.test --nbval  $(ls */*/*ipynb | grep -v 460) | tee pytest.log
+
+
+# py.test --nbval JIRA/ATO-457/distortionConvergenceStudy.ipynb - needed to fix osbolte bokehDrawPanda
