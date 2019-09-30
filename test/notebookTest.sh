d264b967399af252087c83b9ce9006e546e86cfa
janthamade <maynak@excite.com>
2019-09-30 16:19:15 +0200

    notebooks are prepared for SWAN

--- test/notebookTest.sh
+++ test/notebookTest.sh
@@ -1,5 +1,11 @@
+#clean_ipynb */*/*ipynb
 for note in $(ls */*/*ipynb); do
-    echo jupyter nbconvert ${note}
+    echo mv ${note}2 ${note}
     jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ${note}
+
 done
-py.test --nbval  $(ls */*/*ipynb | grep -v 460) | tee notebookTest.log
+
+py.test --nbval  $(ls */*/*ipynb | grep -v 460) | tee pytest.log
+
+
+# py.test --nbval JIRA/ATO-457/distortionConvergenceStudy.ipynb - needed to fix osbolte bokehDrawPanda
