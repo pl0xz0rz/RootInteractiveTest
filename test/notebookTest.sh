737ca6781a1d962f8fde7cde8e74c8d8658b85a0
miranov25 <marian.ivanov@cern.ch>
2019-09-30 13:22:52 +0200

    Changing log file name

--- test/notebookTest.sh
+++ test/notebookTest.sh
@@ -1,11 +1,5 @@
-#clean_ipynb */*/*ipynb
 for note in $(ls */*/*ipynb); do
-    echo mv ${note}2 ${note}
+    echo jupyter nbconvert ${note}
     jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ${note}
-
 done
-
-py.test --nbval  $(ls */*/*ipynb | grep -v 460) | tee pytest.log
-
-
-# py.test --nbval JIRA/ATO-457/distortionConvergenceStudy.ipynb - needed to fix osbolte bokehDrawPanda
+py.test --nbval  $(ls */*/*ipynb | grep -v 460) | tee notebookTest.log
