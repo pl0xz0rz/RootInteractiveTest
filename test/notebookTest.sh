4931f9531ac75755a2087506e8febb24dca81062
janthamade <maynak@excite.com>
2019-10-16 17:27:59 +0200

    Update notebookTest.sh

--- test/notebookTest.sh
+++ test/notebookTest.sh
@@ -1,11 +1,6 @@
-#clean_ipynb */*/*ipynb
-for note in $(ls */*/*ipynb); do
-    echo mv ${note}2 ${note}
-    jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ${note}
+for note in $(ls */*/*ipynb); do	for note in $(ls */*/*ipynb); do
+    echo jupyter nbconvert ${note}	    echo mv ${note}2 ${note}
+    jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ${note}	    jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ${note}
 
-done
-
-py.test --nbval  $(ls */*/*ipynb | grep -v 460) | tee pytest.log
-
-
-# py.test --nbval JIRA/ATO-457/distortionConvergenceStudy.ipynb - needed to fix osbolte bokehDrawPanda
+done	done
+py.test --nbval  $(ls */*/*ipynb | grep -v 460) | tee notebookTest.log
