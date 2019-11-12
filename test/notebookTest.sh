7c3fe85ea7a33b1ed4208e18c8c6bbabd1bf36d8
janthamade <maynak@excite.com>
2019-11-12 13:08:49 +0100

    LoadTree implementation

--- test/notebookTest.sh
+++ test/notebookTest.sh
@@ -2,4 +2,4 @@ for note in $(ls */*/*ipynb); do
     echo jupyter nbconvert ${note}
     jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ${note}
 done
-py.test --nbval  $(ls */*/*ipynb | grep -v 460 |grep -v 566|grep -v 8275) | tee notebookTest.log
+py.test --nbval  $(ls */*/*ipynb | grep -v 460) | tee notebookTest.log
