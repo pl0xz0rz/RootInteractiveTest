8ba1f2ad3c678d8072a2c9b70820bb888944eba3
miranov25 <marian.ivanov@cern.ch>
2020-04-29 19:01:59 +0200

    update

---
+++ JIRA/PWGPP-587/fastRICH.py
@@ -0,0 +1,42 @@
+
+def angleRICH(detetoctorParam, p, mass, Z, phi, eta):
+    """
+    :param detetoctorParam:
+    :param p:                 particle momentum
+    :param mass:              particle mass
+    :param Z:                 charge
+    :param phi:               inclination angle in phi
+    :param eta:               inclination angle eta
+    :return:                  angle
+    """
+    return
+
+def angularResolutionRICH(detectorParam, p, mass, phi,eta):
+    """
+    :param detectorParam:
+        1.) aerogelWidthArray,aerogelRefIndexArray  - multi-layer aerogel with increasing refractive index vector:  [[widthi, ni]]
+            * width in cm
+            * ref index
+        2.) expansion gap (in cm)
+        3.) pixel size
+        4.) single photon time resolution
+        5.) amount of Cherenkov photons/cm (derived from )
+        6.) layer radius
+    :param p:
+    :param mass:
+    :param phi:
+    :param eta:
+    :return: xxx
+    """
+    length0=sum()
+    length=length0*sqrt(1+tan(phi)*tan(phi)+ tan(eta)*tan(eta))
+
+    nCherenkov=length
+
+    return
+
+def angleRICHTrack(detectorParam, trackParam):
+    return
+def angularResolutionRICHtrack(detectorParam, trackParam):
+    return
+
