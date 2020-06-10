Divided the hessian by 2, the error estimate now seems to be correct

JIRA/PWGPP-576/fitter_torch.py
@@ -95,13 +95,13 @@ def curve_fit(fitfunc,x,y,params, weights = 1, sigma = None, lossfunc = None, op
     
     if len(params) == 1:
         hessian = torch.autograd.functional.hessian(lambda a:lossfunc(y,fitfunc(x,a),weights,invsigma),params[0])
-        return params,hessian.detach().pinverse()
+        return params,2*hessian.detach().pinverse()
     else:
         hessian = torch.autograd.functional.hessian(lambda *a:lossfunc(y,fitfunc(x,*a),weights,invsigma),tuple(params))
         hessian = torch.stack([torch.stack([hessian[j][i] for i in range(len(params))]) for j in range(len(params))],1)
-        return params,hessian.detach().pinverse()
+        return params,2*hessian.detach().pinverse()
     
-    return params,hessian.detach().pinverse()
+    return params,2*hessian.detach().pinverse()
 
 def curve_fit_BS(x,y,fitfunc,init_params,sigma0=1,weights=None,nbootstrap=50,fitter_options={},fitter_name='Pytorch_LBFGS'):
 