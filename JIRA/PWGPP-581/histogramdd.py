415a88b28eac1313e1f31b8f400de90fc4cc5c3a
Marian Ivanov <majo.ivanov@gmail.com>
2020-07-03 00:14:13 +0200

    Added N-dimensional histogramming using Pytorch

---
+++ JIRA/PWGPP-581/histogramdd.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+
+hsgh
+"""
+
+import torch
+
+
+def histogramdd(sample,bins=None,ranges=None,weights=None,workaround=True):
+    edges=None
+    device=None
+    custom_edges = False
+    D = sample.size(0)
+    if device == None:
+        device = sample.device
+    if bins == None:
+        if edges == None:
+            bins = 10
+            custom_edges = False
+        else:
+            try:
+                bins = edges.size(1)-1
+            except AttributeError:
+                bins = torch.empty(D)
+                for i in range(len(edges)):
+                    bins[i] = edges[i].size(0)-1
+                bins = bins.to(device)
+            custom_edges = True
+    try:
+        M = bins.size(0)
+        if M != D:
+            raise ValueError(
+                'The dimension of bins must be equal to the dimension of the '
+                ' sample x.')
+    except AttributeError:
+        # bins is either an integer or a list
+        if type(bins) == int:
+            bins = torch.full([D],bins,dtype=torch.long,device=device)
+        elif torch.is_tensor(bins[0]):
+            custom_edges = True
+            edges = bins
+            bins = torch.empty(D,dtype=torch.long)
+            for i in range(len(edges)):
+                bins[i] = edges[i].size(0)-1
+            bins = bins.to(device)
+        else:
+            bins = torch.as_tensor(bins)
+    if bins.dim() == 2:
+        custom_edges = True
+        edges = bins
+        bins = torch.full([D],bins.size(1)-1,dtype=torch.long,device=device)
+    if custom_edges:
+        if not torch.is_tensor(edges):
+            m = max(i.size(0) for i in edges)
+            tmp = torch.empty([D,m],device=edges[0].device)
+            for i in range(D):
+                s = edges[i].size(0)
+                tmp[i,:]=edges[i][-1]
+                tmp[i,:s]=edges[i][:]
+            edges = tmp.to(device)
+        k = torch.searchsorted(edges,sample)
+        k = torch.min(k,(bins+1).reshape(-1,1))
+    else:
+        if workaround:
+            if ranges == None:
+                ranges = torch.empty(2,D,device=device)
+                ranges[0,:]=torch.min(sample,1)[0]
+                ranges[1,:]=torch.max(sample,1)[0]
+            tranges = torch.empty_like(ranges)
+            tranges[1,:] = bins/(ranges[1,:]-ranges[0,:])
+            tranges[0,:] = 1-ranges[0,:]*tranges[1,:]
+            k = torch.addcmul(tranges[0,:].reshape(-1,1),sample,tranges[1,:].reshape(-1,1)).long() #Get the right index
+            k = torch.max(k,torch.zeros([],device=device,dtype=torch.long)) #Underflow bin
+            k = torch.min(k,(bins+1).reshape(-1,1))
+        else:
+            if ranges == None:
+                k = findbin_uniform(sample,bins)
+            else:
+                k = findbin_uniform(sample,bins,ranges)
+
+
+
+    multiindex = torch.ones_like(bins)
+    multiindex[1:] = torch.cumprod(torch.flip(bins[1:],[0])+2,-1).long()
+    multiindex = torch.flip(multiindex,[0])
+    l = torch.sum(k*multiindex.reshape(-1,1),0)
+    hist = torch.bincount(l,minlength=(multiindex[0]*(bins[0]+2)).item(),weights=weights)
+    hist = hist.reshape(tuple(bins+2))
+    return hist
---
+++ JIRA/PWGPP-581/histogramtest.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 14:45:57 2020
+
+@author: majoi
+"""
+
+import numpy as np
+import torch
+import histogramdd
+import time
+
+torch.random.manual_seed(19680801)
+
+sample = torch.arange(0,1,.01)*torch.arange(1,4).reshape([-1,1]) % 1
+histnp = np.histogramdd(sample.transpose(0,1))[0]
+hist = histogramdd(sample)
+print(hist[1:-1,1:-1,1:-1]-histnp)
+
+sample = torch.rand(3,100000)
+histnp = np.histogramdd(sample.transpose(0,1),6)[0]
+hist = histogramdd(sample,6)
+print(hist[1:-1,1:-1,1:-1]-histnp)
+
+sample = torch.rand(3,100000)
+histnp = np.histogramdd(sample.transpose(0,1),[4,5,6])[0]
+hist = histogramdd(sample,[4,5,6])
+print(hist[1:-1,1:-1,1:-1]-histnp)
+
+sample = torch.rand(3,100000)
+ranges = torch.empty(2,3)
+ranges[0,:] = .2
+ranges[1,:] = .8
+histnp = np.histogramdd(sample.transpose(0,1),[4,5,6],[[.2,.8],[.2,.8],[.2,.8]])[0]
+hist = histogramdd(sample,[4,5,6],ranges)
+print(hist[1:-1,1:-1,1:-1]-histnp)
+
+sample = torch.rand(3,100000)
+bins = [torch.rand([8]).sort()[0],torch.rand([7]).sort()[0],torch.rand([6]).sort()[0]]
+histnp = np.histogramdd(sample.transpose(0,1),bins)[0]
+hist = histogramdd(sample,bins)
+print(hist[1:-1,1:-1,1:-1]-histnp)
+
+sample = torch.rand(2,10000000)
+csample = sample.cuda()
+t0 = time.time()
+hist = histogramdd(sample,2)
+print('CPU: histogramdd in %0.3fms' % (1000*(time.time()-t0)))
+torch.cuda.synchronize()
+t0 = time.time()
+histc = histogramdd(csample,2)
+torch.cuda.synchronize()
+print('GPU: histogramdd in %0.3fms' % (1000*(time.time()-t0)))
+dif = hist - histc.to('cpu')
+print(dif)
+
+sample = torch.zeros(2,10000000)
+sample[:,0] = 1
+csample = sample.cuda()
+t0 = time.time()
+hist = histogramdd(sample,2)
+print('CPU: histogramdd in %0.3fms' % (1000*(time.time()-t0)))
+torch.cuda.synchronize()
+t0 = time.time()
+histc = histogramdd(csample,2)
+torch.cuda.synchronize()
+print('GPU: histogramdd in %0.3fms' % (1000*(time.time()-t0)))
+dif = hist - histc.to('cpu')
+print(dif)
